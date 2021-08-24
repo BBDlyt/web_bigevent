cba87e573785b3c480de7132a47af960afc73c74
lytcoder <1564475810@qq.com>
2021-08-24 11:59:15 +0800

    完成了主页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,10 +1,17 @@
 //每次发送请求，会先调用ajaxPrefilter这个函数，可以拿到给ajax提供的配置对象
-$.ajaxPrefilter(function(option){
-   
-    option.url='http://api-breakingnews-web.itheima.net'+option.url
-    if(option.url.indexOf('/my')!==-1){
-         option.headers={
-Authorization:localStorage.getItem('token')
+$.ajaxPrefilter(function (option) {
+
+    option.url = 'http://api-breakingnews-web.itheima.net' + option.url
+    if (option.url.indexOf('/my') !== -1) {
+        option.headers = {
+            Authorization: localStorage.getItem('token')
+        }
+
     }
-}
+    option.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -26,13 +26,13 @@ $(function(){
                 }
                 renderAvater(res.data)
             },
-            complete:function(res){
-                if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
-                    localStorage.removeItem('token')
-                    location.href='/login.html'
-                }
+            // complete:function(res){
+            //     if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
+            //         localStorage.removeItem('token')
+            //         location.href='/login.html'
+            //     }
                 
-            }
+            // }
         })
 
     }
